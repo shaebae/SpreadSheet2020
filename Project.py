523e3d09dc9447f88c842595b0be9a0cb26ab6f0
shaebae <56270188+shaebae@users.noreply.github.com>
2020-04-25 22:35:46 +0100

    add monthly changes

--- Project.py
+++ Project.py
@@ -13,20 +13,37 @@ def read_data():
 def run():
     data = read_data()
 
+    months = []
+    for row in data:
+            month = (row['month'])
+            months.append(month)
+
     sales = []
     for row in data:
             sale = int(row['sales'])
             sales.append(sale)
-
     total = sum(sales)
+    length = len(sales)
+    average = sum(sales)/len(sales)
+
+    monthly_changes = []
+    for i in range(11):
+        monthly_change = ((float(sales[i + 1])/float(sales[i])-1)*100)
+        monthly_changes.append(monthly_change)
+
+    for i in range(11):
+        print(months[i +1], format(monthly_changes[i]/100,".0%"))
+
+    print("Monthly percentage changes:")
+
+
     print('Monthly Sales: {}'.format(sales))
     print('Total sales: {}'.format(total))
+    print('Length list:{}'.format(length))
+    print('Average Sales:{}'.format(average))
 
-
-run()
-
-print("Test")
-
-print('Test2? ')
-
-print("Hello")
+    min_sales = min(sales)
+    print('Minimum Sales: {}'.format(min_sales))
+    max_sales = max(sales)
+    print('Maximum sales: {}'.format(max_sales))
+run ()
