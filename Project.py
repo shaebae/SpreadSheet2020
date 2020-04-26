3c355ac44d6398db8eb61dfcd9c400f3501e2a59
shaebae <56270188+shaebae@users.noreply.github.com>
2020-04-26 15:25:28 +0100

    adding excel code to Practice

--- Project.py
+++ Project.py
@@ -1,4 +1,9 @@
 import csv
+import xlsxwriter
+
+
+workbook = xlsxwriter.Workbook('data.xlsx')
+worksheet = workbook.add_worksheet()
 
 def read_data():
     data=[]
@@ -13,15 +18,21 @@ def read_data():
 def run():
     data = read_data()
 
-    months = []
-    for row in data:
-            month = (row['month'])
-            months.append(month)
-
     sales = []
     for row in data:
             sale = int(row['sales'])
             sales.append(sale)
+
+    months = []
+    for row in data:
+        month = (row['month'])
+        months.append(month)
+
+    expenditure = []
+    for row in data:
+        expenditures = int(row['expenditure'])
+        expenditure.append(expenditures)
+
     total = sum(sales)
     length = len(sales)
     average = sum(sales)/len(sales)
@@ -31,11 +42,9 @@ def run():
         monthly_change = ((float(sales[i + 1])/float(sales[i])-1)*100)
         monthly_changes.append(monthly_change)
 
+    print("Monthly percentage changes:")
     for i in range(11):
         print(months[i +1], format(monthly_changes[i]/100,".0%"))
-
-    print("Monthly percentage changes:")
-
 
     print('Monthly Sales: {}'.format(sales))
     print('Total sales: {}'.format(total))
@@ -46,4 +55,34 @@ def run():
     print('Minimum Sales: {}'.format(min_sales))
     max_sales = max(sales)
     print('Maximum sales: {}'.format(max_sales))
+
+    worksheet.write(0, 0, 'Month')
+    row = 1
+    for item in months:
+        worksheet.write(row, 0, item)
+        row = row + 1
+
+    worksheet.write(0, 1, 'Sales')
+    row = 1
+    for item in sales:
+        worksheet.write(row, 1, item)
+        row = row + 1
+
+    worksheet.write(0, 2, 'Expenditures')
+    row = 1
+    for item in expenditure:
+        worksheet.write(row, 2, item)
+        row = row + 1
+
+    worksheet.write(0, 3, 'Monthly Sales Changes %')
+    row = 2
+    for item in monthly_changes:
+        worksheet.write(row, 3, format(item/100,".0%"))
+        row = row + 1
 run ()
+
+
+
+#and add the results in other rows
+
+workbook.close()
